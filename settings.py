9abece0f8021760feb3645dc1c6ccb8ca9abf1c4
Andy Babic <ababic@rkh.co.uk>
2018-06-29 17:19:49 +0100

    Add a link to the article where Guido talks about the sys.modules hack

--- settings.py
+++ settings.py
@@ -7,4 +7,5 @@ class MyAppSettingsHelper(BaseAppSettingsHelper):
     pass
 
 
+# See: https://mail.python.org/pipermail/python-ideas/2012-May/014969.html
 sys.modules[__name__] = MyAppSettingsHelper()
